35dab092cf06e4138adeb19024ea4056ce9d76e0
Nikita Averianov <n.averianov@intensa.ru>
2025-01-22 17:06:19 +0300

    refactor: оптимизировать код, добавить стили

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
-import {Dog, Cat, Cow, Developer} from "./animal/Animal.js";
+import { Dog, Cat, Cow, Developer } from "./animal/Animal.js";
 
 const dog = new Dog();
 const cat = new Cat();
 const cow = new Cow();
 const developer = new Developer();
 
-const dogButton = document.getElementById('dogButton');
-const catButton = document.getElementById('catButton');
-const cowButton = document.getElementById('cowButton');
-const devButton = document.getElementById('devButton');
+const buttons = document.querySelector(".animals__buttons");
 
-const voiceContainer = document.getElementById('voiceContainer');
+const dogButton = buttons.querySelector("#dogButton");
+const catButton = buttons.querySelector("#catButton");
+const cowButton = buttons.querySelector("#cowButton");
+const devButton = buttons.querySelector("#devButton");
+
+const voiceContainer = document.getElementById("voiceContainer");
 
 function showAnimalVoice(animal, name) {
 	voiceContainer.textContent = animal.speak(name);
 }
 
-dogButton.addEventListener('click', () => {showAnimalVoice(dog, "Шарик")});
-catButton.addEventListener('click', () => {showAnimalVoice(cat, "Мурка")});
-cowButton.addEventListener('click', () => {showAnimalVoice(cow, "Бурёнка")});
-devButton.addEventListener('click', () => {showAnimalVoice(developer, "Вовочка")});
+dogButton.addEventListener("click", () => {
+	showAnimalVoice(dog, "Шарик");
+});
+catButton.addEventListener("click", () => {
+	showAnimalVoice(cat, "Мурка");
+});
+cowButton.addEventListener("click", () => {
+	showAnimalVoice(cow, "Бурёнка");
+});
+devButton.addEventListener("click", () => {
+	showAnimalVoice(developer, "Вовочка");
+});
